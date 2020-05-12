aaf4cc3528470c1af1245d213bf2f48259be5e49
cybercatlab <cybercatlab@mail.com>
2020-05-12 13:40:57 +0300

    v3

---
+++ js/main.js
@@ -0,0 +1,21 @@
+ $(document).ready(function(){
+    $(".navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
